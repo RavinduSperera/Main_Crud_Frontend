2ae0387eb8a9b703ac55d0aa53c7a4ae267bbd3b
RavinduSperera <it22064622@my.sliit.lk>
2024-04-06 16:05:07 +0530

    Created Folders and files

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import PlaceOrder from './pages/PlaceOrder'
+
+export default function App() {
+  return (
+    <div>
+      <PlaceOrder/>
+    </div>
+  )
+}
---
+++ src/pages/PlaceOrder.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import OrderFrom from '../components/Place Order Comps/OrderForm'
+
+export default function PlaceOrder() {
+  return (
+    <div>
+        <OrderFrom/>
+    </div>
+  )
+}
